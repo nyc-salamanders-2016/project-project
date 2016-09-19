2197eb108f032ec98aa101f3490136fbec3e093d
Daniel Yoon & Melissa Rudofsky <pair+dyoon3102.mrudof@devbootcamp.com>
2016-09-19 16:17:43 -0400

    fixed conclifts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+	def new
+		@cohorts = Cohort.all.to_json
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect '/'
+		else
+			status 422
+			render json: @user.errors.full_messages
+		end
+	end
+
+	private
+
+  def user_params
+    params.require(:user).permit( :username, :email, :password, :admin, :cohort_id, :team_id )
+  end
+
 end
