75ba400e56efb2461070518a622cd3165f692329
Terence Stone <email": null>
2016-09-19 12:19:52 -0400

    Set up migrations

--- db/migrate/20160919160535_create_rankings.rb
+++ db/migrate/20160919160535_create_rankings.rb
@@ -1,6 +1,9 @@
 class CreateRankings < ActiveRecord::Migration[5.0]
   def change
     create_table :rankings do |t|
+    	t.integer :rank, null: false
+    	t.integer :user_id, null: false
+    	t.integer :pitch_id, null: false
 
       t.timestamps
     end
--- db/migrate/20160919160542_create_cohorts.rb
+++ db/migrate/20160919160542_create_cohorts.rb
@@ -1,7 +1,9 @@
 class CreateCohorts < ActiveRecord::Migration[5.0]
   def change
     create_table :cohorts do |t|
-
+    	t.string :name, null:false
+    	t.integer, :year, null:false
+    	
       t.timestamps
     end
   end
