c384d6ea03a1e05bd9cf370db8af8171bba96b88
Daniel Yoon & Melissa Rudofsky <pair+dyoon3102.mrudof@devbootcamp.com>
2016-09-19 17:09:18 -0400

    created register, login, logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	get    '/login',   to: 'login#new'
+  post   '/login',   to: 'login#create'
+  delete '/logout',  to: 'login#destroy'
+  
 	resources :users, only: [:new, :create]
 	resources :pitches
 	resources :teams
